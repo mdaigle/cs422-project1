22878c7696cd466a6d9c2de478a4c77b3c82c0a6
Matt Olma <matthaios.olma@epfl.ch>
2018-02-28 22:04:31 +0100

    Initial push

---
+++ src/main/java/ch/epfl/dias/store/column/DBColumn.java
@@ -0,0 +1,12 @@
+package ch.epfl.dias.store.column;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import ch.epfl.dias.store.DataType;
+
+public class DBColumn {
+
+	// TODO: Implement
+
+}
